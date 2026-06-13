b57a4bf7e527446ec353a2b7bee7e7b6d3196eb3
giahung123456 <dogiahungtplc@gmail.com>
2026-06-13 17:11:09 +0700

    taskkkkkkkkkkkk3

--- src/main/java/com/example/prj_job_and_recruitment_exchange_system/controller/EmployerApplicationController.java
+++ src/main/java/com/example/prj_job_and_recruitment_exchange_system/controller/EmployerApplicationController.java
@@ -1,4 +1,21 @@
 package com.example.prj_job_and_recruitment_exchange_system.controller;
 
+import com.example.prj_job_and_recruitment_exchange_system.model.request.UpdateStatusRequest;
+import com.example.prj_job_and_recruitment_exchange_system.service.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/employer/applications")
+@RequiredArgsConstructor
 public class EmployerApplicationController {
+
+    private final ApplicationService applicationService;
+
+    @PutMapping("/update-status")
+    public ResponseEntity<String> updateStatus(@RequestBody UpdateStatusRequest request) {
+        applicationService.updateApplicationStatus(request);
+        return ResponseEntity.ok("Cập nhật trạng thái hồ sơ ứng tuyển và gửi mail phản hồi thành công!");
+    }
 }
